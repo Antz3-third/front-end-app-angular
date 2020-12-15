7ed6742c60b90e7bbdea8ca8db34e821a57502d4
Antz3-third <aperryiii20@gmail.com>
2020-12-15 13:29:59 -0800

    comment update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,16 +8,20 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public readonly loginForm: FormGroup;
+  private readonly loginForm: FormGroup;
   constructor(private fb: FormBuilder) { 
-    this.loginForm = this.fb.group({
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required, Validators.minLength(8)])
-  });
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    }); 
+
 }
 
   ngOnInit(): void {
     
   }
 
+  get email() {return this.loginForm.get('email');}
+
+  get password() {return this.loginForm.get('password');}
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  private readonly registerForm: FormGroup;
+  constructor(private fb: FormBuilder) { 
+    this.registerForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      retypePassword: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      birthYear: new FormControl(null, [Validators.required, Validators.maxLength(4)])
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  get email() {return this.registerForm.get('email');}
+
+  get password() {return this.registerForm.get('password');}
+
+  get retypePassword() {return this.registerForm.get('retypePassword');}
+
+  get birthYear() {return this.registerForm.get('birthYear');}
 }
+
